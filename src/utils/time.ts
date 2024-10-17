899266fe72bb31184a53f881798e2abb61103133
julioale21 <30907777+julioale21@users.noreply.github.com>
2024-10-17 20:54:24 -0300

    added lottie animation

---
+++ src/utils/time.ts
@@ -0,0 +1,6 @@
+export const formatTimeFunction = (time: number) => {
+  const seconds = Math.floor(time / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${minutes}:${remainingSeconds.toString().padStart(2, "0")}`;
+};
