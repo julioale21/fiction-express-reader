3485630acb0f06a39b2962d7ed30f15da244aa55
julioale21 <30907777+julioale21@users.noreply.github.com>
2024-10-19 01:32:29 -0300

    updated providers

--- src/config/providers/providers.tsx
+++ src/config/providers/providers.tsx
@@ -1,12 +1,13 @@
 "use client";
-
+import React from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { ReadingMetricsProvider } from "@/app/books/contexts/ReadingMetricsContext";
 import SessionAuthProvider from "../context/SessionAuthProvider";
 import theme from "../themes/theme";
 import getQueryClient from "../client/getQueryClient";
+
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export function Providers({ children }: { children: React.ReactNode }) {
