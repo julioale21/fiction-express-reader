0d3991efa54528d681c353cc9481b076e272a82f
julioale21 <30907777+julioale21@users.noreply.github.com>
2024-10-20 00:51:25 -0300

    fixed public assets

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,23 @@
-
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
+
+// Define a list of file extensions that are typically public assets
+const publicFileExtensions = [
+  "css",
+  "js",
+  "json",
+  "jpg",
+  "jpeg",
+  "png",
+  "gif",
+  "svg",
+  "ico",
+  "woff",
+  "woff2",
+  "ttf",
+  "eot",
+];
 
 export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
@@ -9,12 +25,17 @@ export async function middleware(request: NextRequest) {
   const publicPaths = ["/", "/auth/login", "/auth/register"];
   const isPublicPath = publicPaths.some((publicPath) => path === publicPath);
 
+  // Check if the path is a public asset
+  const isPublicAsset = publicFileExtensions.some((ext) =>
+    path.endsWith(`.${ext}`)
+  );
+
   const token = await getToken({
     req: request,
     secret: process.env.NEXTAUTH_SECRET,
   });
 
-  if (!isPublicPath && !token) {
+  if (!isPublicPath && !isPublicAsset && !token) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
 
