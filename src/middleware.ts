e1a19981b8badd187cceef8a16fd0343d101f269
julioale21 <30907777+julioale21@users.noreply.github.com>
2024-10-20 17:43:24 -0300

    updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,41 @@
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+// import { getToken } from "next-auth/jwt";
 
+// export async function middleware(request: NextRequest) {
+//   const path = request.nextUrl.pathname;
+
+//   // Define las rutas públicas
+//   const publicPaths = ["/", "/auth/login", "/auth/register"];
+//   const isPublicPath = publicPaths.includes(path);
+
+//   // Define las rutas que deben ser excluidas del middleware
+//   const excludedPaths = ["/_next", "/api/auth"];
+//   const isExcludedPath = excludedPaths.some((excludedPath) =>
+//     path.startsWith(excludedPath)
+//   );
+
+//   // Comprueba si la ruta es un archivo estático
+//   const isStaticFile = /\.(.*)$/.test(path);
+
+//   // Obtiene el token de autenticación
+//   const token = await getToken({
+//     req: request,
+//     secret: process.env.NEXTAUTH_SECRET,
+//   });
+
+//   // Si la ruta no es pública, no está excluida, no es un archivo estático, y no hay token, redirige al login
+//   if (!isPublicPath && !isExcludedPath && !isStaticFile && !token) {
+//     return NextResponse.redirect(new URL("/auth/login", request.url));
+//   }
+
+//   return NextResponse.next();
+// }
+
+// // Configura el matcher para incluir todas las rutas excepto las excluidas
+// export const config = {
+//   matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico).*)"],
+// };
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
@@ -6,34 +43,28 @@ import { getToken } from "next-auth/jwt";
 export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
 
-  // Define las rutas públicas
   const publicPaths = ["/", "/auth/login", "/auth/register"];
-  const isPublicPath = publicPaths.includes(path);
-
-  // Define las rutas que deben ser excluidas del middleware
-  const excludedPaths = ["/_next", "/api/auth"];
-  const isExcludedPath = excludedPaths.some((excludedPath) =>
-    path.startsWith(excludedPath)
+  const isPublicPath = publicPaths.some((publicPath) =>
+    path.startsWith(publicPath)
   );
 
-  // Comprueba si la ruta es un archivo estático
-  const isStaticFile = /\.(.*)$/.test(path);
+  const excludedDirectories = ["/_next/", "/public/"];
+  const isExcludedDirectory = excludedDirectories.some((excludedDirectory) =>
+    path.startsWith(excludedDirectory)
+  );
 
-  // Obtiene el token de autenticación
   const token = await getToken({
     req: request,
     secret: process.env.NEXTAUTH_SECRET,
   });
 
-  // Si la ruta no es pública, no está excluida, no es un archivo estático, y no hay token, redirige al login
-  if (!isPublicPath && !isExcludedPath && !isStaticFile && !token) {
+  if (!isPublicPath && !isExcludedDirectory && !token) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
   }
 
   return NextResponse.next();
 }
 
-// Configura el matcher para incluir todas las rutas excepto las excluidas
 export const config = {
-  matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next|public|auth|login|register).*)"],
 };
