33bd2e29c1cac1350cc407d34424572170e14aca
julioale21 <30907777+julioale21@users.noreply.github.com>
2024-10-20 18:01:29 -0300

    updated middleware test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,3 @@
-
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
@@ -11,7 +10,15 @@ export async function middleware(request: NextRequest) {
   const isPublicPath = publicPaths.includes(path);
 
   // Define las rutas que deben ser excluidas del middleware
-  const excludedPaths = ["/_next", "/api/auth"];
+  const excludedPaths = [
+    "/_next",
+    "/api/auth",
+    "/public",
+    "/static",
+    "/dist",
+    "/.next",
+    "/vercel",
+  ];
   const isExcludedPath = excludedPaths.some((excludedPath) =>
     path.startsWith(excludedPath)
   );
@@ -35,5 +42,7 @@ export async function middleware(request: NextRequest) {
 
 // Configura el matcher para incluir todas las rutas excepto las excluidas
 export const config = {
-  matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/((?!api/auth|_next/static|_next/image|favicon.ico|public|static|dist|\\.next|vercel).*)",
+  ],
 };
